c53e6b581aa731e0a1599b3b48eef352d77f4783
Mickey <mickeyc11111@gmail.com>
2022-04-11 19:01:04 -0400

    commit tree d9097f5c143ce3f679ffe7bbd170e6ac022fb22c

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b;
+}
+
+function increment(n){
+    n += 1
+    return n;
+}
+function decrement(n){
+    n -= 1
+    return n;
+}
+
+function makeInt(n){
+    const parsed = parseInt(n, 10);
+    return parsed;
+}
+
+function preserveDecimal(n){
+    const parsed = parseFloat(n);
+    return parsed;
+}
